01da2eb9836b65784c9dcb7f74ff4f3829e6c28e
tomoyukikoyanagi <tomoyukikoyanagi@gmail.com>
2021-09-07 08:29:22 +0900

    Initial Commit

---
+++ carplay_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  carplay_tutorial
+//
+//  Created by 小柳智之 on 2021/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
